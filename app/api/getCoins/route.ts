8e5fb5457aa4f877d30df4a63128226567500261
shivimishra <shivimishra130103@gmail.com>
2024-03-05 20:08:28 +0530

    fetch trending coins

---
+++ app/api/getCoins/route.ts
@@ -0,0 +1,34 @@
+type CoinData = {
+    name: string;
+    symbol: string;
+    priceChangePercentage: {
+      usd: number;
+    };
+  };
+
+export async function GET(request: Request) {
+  try {
+    const response = await fetch(
+      "https://api.coingecko.com/api/v3/search/trending"
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch trending coins");
+    }
+
+    const data = await response.json();
+    
+    const top3TrendingCoins: CoinData[] = data.coins.slice(0, 3).map((coin: any) => ({
+        name: coin.item.name,
+        symbol: coin.item.symbol,
+        priceChangePercentage: coin.item.data.price_change_percentage_24h.usd,
+      }));
+    
+      return new Response(JSON.stringify(top3TrendingCoins), {
+        headers: { "Content-Type": "application/json" },
+      });
+  } catch (error: any) {
+    console.error("Error fetching trending coins:", error.message);
+    throw new Error("Internal Server Error");
+  }
+}
